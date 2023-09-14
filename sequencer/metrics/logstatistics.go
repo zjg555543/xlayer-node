cd9fe21bff6692d194dfb375719ef772f73e108f
zhangkai <zhangkai.gis@163.com>
2023-09-14 16:47:58 +0800

    add log statistics

---
+++ sequencer/metrics/logstatistics.go
@@ -0,0 +1,38 @@
+package metrics
+
+import (
+	"time"
+)
+
+type LogTag string
+
+type LogStatistics interface {
+	CumulativeCounting(tag LogTag)
+	CumulativeTiming(tag LogTag, duration time.Duration)
+	SetTag(tag LogTag, value string)
+	Summary() string
+	ResetStatistics()
+
+	UpdateTimestamp(tag LogTag, tm time.Time)
+}
+
+const (
+	TxCounter             LogTag = "TxCounter"
+	GetTx                 LogTag = "GetTx"
+	GetTxPauseCounter     LogTag = "GetTxPauseCounter"
+	BatchCloseReason      LogTag = "BatchCloseReason"
+	ReprocessingTxCounter LogTag = "ReProcessingTxCounter"
+	FailTxCounter         LogTag = "FailTxCounter"
+	NewRound              LogTag = "NewRound"
+
+	ProcessingTxTiming         LogTag = "ProcessingTxTiming"
+	ProcessingInvalidTxCounter LogTag = "ProcessingInvalidTxCounter"
+	ProcessingTxCommit         LogTag = "ProcessingTxCommit"
+	ProcessingTxResponse       LogTag = "ProcessingTxResponse"
+
+	FinalizeBatchTiming             LogTag = "FinalizeBatchTiming"
+	FinalizeBatchNumber             LogTag = "FinalizeBatchNumber"
+	FinalizeBatchReprocessFullBatch LogTag = "FinalizeBatchReprocessFullBatch"
+	FinalizeBatchCloseBatch         LogTag = "FinalizeBatchCloseBatch"
+	FinalizeBatchOpenBatch          LogTag = "FinalizeBatchOpenBatch"
+)
