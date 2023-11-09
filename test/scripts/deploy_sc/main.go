3e0be81cab5b9f2882e7df5872eddfb233198b19
jianguo <zjg555543@163.com>
2023-11-09 18:31:41 +0800

    update

--- test/scripts/deploy_sc/main.go
+++ test/scripts/deploy_sc/main.go
@@ -132,14 +132,12 @@ func main() {
 		log.Debugf("Sending TX to transfer ETH")
 		to := common.HexToAddress(receiverAddr)
 		tx = ethTransfer(ctx, client, auth, to, transferAmount, nil)
-		fmt.Println()
-
+		err = operations.WaitTxToBeMined(ctx, client, tx, txTimeout)
+		chkErr(err)
 		// Invalid ETH Transfer
 		log.Debugf("Sending Invalid TX to transfer ETH")
 		nonce := tx.Nonce() + 1
 		ethTransfer(ctx, client, auth, to, transferAmount, &nonce)
-		err = operations.WaitTxToBeMined(ctx, client, tx, txTimeout)
-		chkErr(err)
 		fmt.Println()
 	}
 }
