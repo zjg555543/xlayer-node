d8f956b3c4e6bcb8d0e2f4acb6a3ed3e4492a390
lyh169 <liuyuhang203@sina.com>
2023-11-10 16:26:44 +0800

    fix the ut

--- test/e2e/debug_test.go
+++ test/e2e/debug_test.go
@@ -519,7 +519,7 @@ func TestDebugTraceBlock(t *testing.T) {
 		},
 	}
 
-	results := map[string]json.RawMessage{}
+	results := map[string][]interface{}{}
 
 	type testCase struct {
 		name              string
@@ -599,28 +599,26 @@ func TestDebugTraceBlock(t *testing.T) {
 				require.Nil(t, response.Error)
 				require.NotNil(t, response.Result)
 
-				results[network.Name] = response.Result
+				resultTransactions := []interface{}{}
+				err = json.Unmarshal(response.Result, &resultTransactions)
+				require.NoError(t, err)
+
+				results[network.Name] = []interface{}{resultTransactions[receipt.TransactionIndex]}
 			}
 
-			referenceTransactions := []interface{}{}
-			err = json.Unmarshal(results[l1NetworkName], &referenceTransactions)
-			require.NoError(t, err)
+			referenceTransactions := results[l1NetworkName]
 
 			for networkName, result := range results {
 				if networkName == l1NetworkName {
 					continue
 				}
 
-				resultTransactions := []interface{}{}
-				err = json.Unmarshal(result, &resultTransactions)
-				require.NoError(t, err)
-
 				for transactionIndex := range referenceTransactions {
 					referenceTransactionMap := referenceTransactions[transactionIndex].(map[string]interface{})
 					referenceResultMap := referenceTransactionMap["result"].(map[string]interface{})
 					referenceStructLogsMap := referenceResultMap["structLogs"].([]interface{})
 
-					resultTransactionMap := resultTransactions[transactionIndex].(map[string]interface{})
+					resultTransactionMap := result[transactionIndex].(map[string]interface{})
 					resultResultMap := resultTransactionMap["result"].(map[string]interface{})
 					resultStructLogsMap := resultResultMap["structLogs"].([]interface{})
 
