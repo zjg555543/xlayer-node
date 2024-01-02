1f0d4334d487590415dbca9b23bf116854b9e877
jianguo <zjg555543@163.com>
2024-01-02 12:16:12 +0800

    fix ut

--- config/default.go
+++ config/default.go
@@ -150,7 +150,6 @@ L2Coinbase = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"
 [SequenceSender]
 WaitPeriodSendSequence = "5s"
 LastBatchVirtualizationTimeMaxWaitPeriod = "5s"
-MaxTxSizeForL1 = 131072
 MaxBatchesForL1 = 10
 PrivateKey = {Path = "/pk/sequencer.keystore", Password = "testonly"}
 UseValidium = true
