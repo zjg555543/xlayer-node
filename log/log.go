15610b87af938fd1c9671596755621243e8537cc
trunghai95 <bthai.1995@gmail.com>
2024-01-09 17:27:50 +0800

    Inject logger to context

--- log/log.go
+++ log/log.go
@@ -48,6 +48,10 @@ func getDefaultLog() *Logger {
 	return log.Load()
 }
 
+func GetDefaultLog() *Logger {
+	return getDefaultLog()
+}
+
 // Init the logger with defined level. outputs defines the outputs where the
 // logs will be sent. By default outputs contains "stdout", which prints the
 // logs at the output of the process. To add a log file as output, the path
