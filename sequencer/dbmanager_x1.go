14350b382f37d2c04d29caf308c2e20994bdecde
chunfengSun <516108736@qq.com>
2024-01-31 10:43:06 +0800

    Part5: pr#56 metric (#86)
    * add metric
    
    * Part6: pr#60 ratelimit (#87)
    
    * small fix (#88)

---
+++ sequencer/dbmanager_x1.go
@@ -0,0 +1,21 @@
+package sequencer
+
+import (
+	"github.com/0xPolygonHermez/zkevm-node/log"
+	"github.com/0xPolygonHermez/zkevm-node/sequencer/metrics"
+	"time"
+)
+
+var countinterval = 10
+
+func (d *dbManager) countPendingTx() {
+	for {
+		<-time.After(time.Second * time.Duration(countinterval))
+		transactions, err := d.txPool.CountPendingTransactions(d.ctx)
+		if err != nil {
+			log.Errorf("load pending tx from pool: %v", err)
+			continue
+		}
+		metrics.PendingTxCount(int(transactions))
+	}
+}
