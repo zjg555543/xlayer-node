ee46e742638129eaad58c90579accc38334b17cc
LeoGuo621 <335209779@qq.com>
2024-02-21 00:01:55 +0800

    Update interfaces.go

--- gasprice/interfaces.go
+++ gasprice/interfaces.go
@@ -5,10 +5,9 @@ import (
 	"math/big"
 	"time"
 
+	"github.com/0xPolygonHermez/zkevm-node/pool"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/jackc/pgx/v4"
-
-	"github.com/0xPolygonHermez/zkevm-node/pool"
 )
 
 // Consumer interfaces required by the package.
