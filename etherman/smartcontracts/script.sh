fd14bf6ad2f337728148e4d407e1a29cade57ff8
JianGuo <zjg555543@163.com>
2024-03-20 14:56:33 +0800

    update

--- etherman/smartcontracts/script.sh
+++ etherman/smartcontracts/script.sh
@@ -8,18 +8,15 @@ gen() {
     abigen --bin bin/${package}.bin --abi abi/${package}.abi --pkg=${package} --out=${package}/${package}.go
 }
 
-#gen oldpolygonzkevmglobalexitroot
-#gen oldpolygonzkevmbridge
-#gen oldpolygonzkevm
-#gen etrogpolygonzkevm
-#gen polygonzkevm
-#gen polygonzkevmbridge
-#gen pol
-#gen polygonzkevmglobalexitroot
-#gen polygonrollupmanager
-#gen mockpolygonrollupmanager
-#gen mockverifier
-#gen proxy
-
-gen polygondatacommittee_xlayer
-gen polygonvalidium_xlayer
+gen oldpolygonzkevmglobalexitroot
+gen oldpolygonzkevmbridge
+gen oldpolygonzkevm
+gen etrogpolygonzkevm
+gen polygonzkevm
+gen polygonzkevmbridge
+gen pol
+gen polygonzkevmglobalexitroot
+gen polygonrollupmanager
+gen mockpolygonrollupmanager
+gen mockverifier
+gen proxy
